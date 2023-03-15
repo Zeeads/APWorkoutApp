b553634ca6c4512364ea0d7656315c087c4ae4ef
Zeeads <zssowelam23@gmail.com>
2023-03-15 09:11:43 -0500

    Initial Commit

---
+++ APWorkoutApp/APWorkoutAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  APWorkoutAppApp.swift
+//  APWorkoutApp
+//
+//  Created by Zeead Sowelam23 on 3/15/23.
+//
+
+import SwiftUI
+
+@main
+struct APWorkoutAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
